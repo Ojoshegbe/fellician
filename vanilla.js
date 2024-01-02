0f58f0fa08760719fdc0d45bc0e4f348e1f7793e
Ojoshegbe <yunusaadejo20@gmail.com>
2024-01-03 00:26:24 +0100

    1s comit mesaage

---
+++ vanilla.js
@@ -0,0 +1,39 @@
+// const aboutGrid = document.querySelectorAll('.about-grid-cont');
+
+// //javascript lenis
+
+// // const lenis = new Lenis()
+
+// // lenis.on('scroll', ScrollTrigger.update)
+
+// // gsap.ticker.add((time)=>{
+// //   lenis.raf(time * 1000)
+// // })
+
+// // gsap.ticker.lagSmoothing(0)
+
+
+
+// aboutGrid.forEach(about => {
+//     let aboutH = about.getElementsByTagName('h2');
+//     let aboutP = about.getElementsByTagName('p');
+
+//     gsap.set(aboutH, {
+//         xPercent: -100,
+//         opacity: 0
+//     })
+
+//     gsap.set(aboutP, {
+//         xPercent: 100,
+//         opacity: 0
+//     })
+// })
+
+
+const navBar = document.getElementById('nav-bar');
+const nav = document.getElementById('nav');
+
+navBar.addEventListener("click", () => {
+    nav.classList.toggle("active")
+
+})
